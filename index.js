1bec8d2230d67dbf1478fa132294be344b141051
Oleksandr <pokalchukoleksandr@gmail.com>
2024-06-28 22:30:52 +0300

    logical operators

---
+++ index.js
@@ -0,0 +1,28 @@
+//const num = 50;
+
+//switch (num) { 
+  //  case 49:
+    //    console.log('Неверно');
+      //  case 100:
+       //     console.log('Неверно');
+//}
+        //case 50:
+        //   console.log ('В точку!');
+//}
+// const hamburger = 3
+// const fries = 0
+
+// if (1) {
+//     console.log('Я сыт!');
+// }
+// // console.log(hamburger && fries);
+const hamburger = 3
+const fries = 1
+
+if (hamburger === 3 && fries) {
+    console.log('Все сыты!');
+} else {
+    console.log ('Мы уходим!');
+}
+
+
